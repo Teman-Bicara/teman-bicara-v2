f9ba59c6689169bc172dddde9225a44529535b54
kisnak21 <krisnastya21@gmail.com>
2023-02-08 05:42:21 +0700

    initial structure folder

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+    return (
+        <div className='flex min-h-screen items-center justify-center bg-gray-900'>
+            <div className='w-full max-w-lg rounded-lg border-t border-gray-600 bg-gray-800 p-6 shadow'>
+                <h4 className='text-2xl text-white'>Hello React</h4>
+                <p className='text-lg leading-relaxed text-gray-400'>
+                    A JavaScript library for building user interfaces
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default App
